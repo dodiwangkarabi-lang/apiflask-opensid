bb92c25080d9c7b9abe70232651db0562e0044a8
dodiwangkarabi-lang <dodiwangkarabi@gmail.com>
2026-07-22 17:03:28 +0800

    add: app config

--- myapp/features/mesin_pencarian/routes.py
+++ myapp/features/mesin_pencarian/routes.py
@@ -1,8 +1,17 @@
 from apiflask import APIBlueprint
+from myapp.config import BASE_DIR
 
 # ----- main -----
 from myapp.features.mesin_pencarian import schema
+
+# ----- use cases -----
 from myapp.features.mesin_pencarian import usecases
+
+# ----- service -----
+from myapp.features.mesin_pencarian import services
+
+# ----- repo -----
+from myapp.features.surat import repository
 
 # ----- auth -----
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -27,3 +36,26 @@ def cari(query_data):
     
     # username = get_jwt_identity()
     # return {"message": "cari", "username": ""}
+    
+@pencarian_bp.get("/cari-surat-masuk/")
+@pencarian_bp.input(schema.PencarianRequestSchema, location="query")
+@pencarian_bp.output(schema.HasilPencarianSchema(many=True))
+@pencarian_bp.doc(security=[{"BearerAuth": []}])
+# @jwt_required()
+def cari_surat_masuk(query_data):
+    repo = repository.SuratMasukRepository()
+    
+    # INDEX_DIR = BASE_DIR / "media" / "index"
+    # search_engine_service = services.SearchEngine(
+    #     index_file=INDEX_DIR / "index_surat_masuk.pkl"
+    # )
+
+    
+    uc = usecases.SearchEngineUseCase(
+        repo=repo,
+        tipe="surat_masuk"
+    )
+    keyword = query_data["q"]
+    hasil_pencarian = uc.search(keyword)
+    
+    return hasil_pencarian
--- myapp/features/mesin_pencarian/usecases/use_cases.py
+++ myapp/features/mesin_pencarian/usecases/use_cases.py
@@ -12,18 +12,46 @@ from myapp.config import BASE_DIR
 
 INDEX_DIR = BASE_DIR / "media" / "index"
 
-search_engine = SearchEngine(
-    index_file = INDEX_DIR / "index.pkl"
-)
-
 
 class SearchEngineUseCase:
-    def __init__(self, search_engine=search_engine, repo=None):
+    def __init__(self, search_engine =None, repo=None, tipe=""):
+        """
+        pencarian use cases
+
+        Args:
+            search_engine (_type_, optional): _description_. Defaults to None.
+            repo (_type_, optional): _description_. Defaults to None.
+            tipe (str, optional): adalah tipe surat (surat masuk, surat keluar, arsip). Defaults to "".
+        """
         self.search_engine = search_engine
         self.repo = repo
         
-    def build(self, documents: list[dict], text_field: str="isi_singkat"):
-        surat_keluar_repo = repository.SuratKeluarRepository()
+        if self.search_engine is None:
+            self._set_search_engine(tipe)
+        
+    def _set_search_engine(self, tipe: str=""):
+        if tipe == "surat_masuk":
+            self.search_engine = SearchEngine(
+                index_file=INDEX_DIR / "index_surat_masuk.pkl"
+            )
+        elif tipe == "surat_keluar":
+            self.search_engine = SearchEngine(
+                index_file=INDEX_DIR / "index.pkl"
+            )
+        elif tipe == "arsip":
+            self.search_engine = SearchEngine(
+                index_file=INDEX_DIR / "index_arsip.pkl"
+            )
+        else:
+            self.search_engine = SearchEngine(
+                index_file=INDEX_DIR / "index.pkl"
+            )
+        
+    
+    def build(self, documents: list[dict] = [], text_field: str="isi_singkat"):
+        if self.repo is None:
+            self.repo = repository.SuratKeluarRepository()
+        
         documents = [
             {
                 "id": surat.id,
@@ -34,11 +62,33 @@ class SearchEngineUseCase:
                 "nomor_urut": surat.nomor_urut,
                 "kode_surat": surat.kode_surat
             }
-            for surat in surat_keluar_repo.get_all()
+            for surat in self.repo.get_all()
         ]
         self.search_engine.build(documents, text_field)
     
-    def search(self, keyword: str, top_k: int = 20) -> list[dict]:
+    def search(self, keyword: str, top_k: int = 20, **kwargs) -> list[dict]:
+        """
+
+        Args:
+            keyword (str): _description_
+            top_k (int, optional): _description_. Defaults to 20.
+            **kwargs: surat_masuk: bool = True
+
+        Returns:
+            list[dict]: _description_
+        """
+        # if self.kwargs.get("surat_keluar"):
+        #     search_engine_service = SearchEngine(
+        #         index_file = INDEX_DIR / "index.pkl"
+        #     )
+        #     self.search_engine = search_engine_service
+        
+        # if kwargs.get("surat_masuk"):
+        #     search_engine_service = SearchEngine(
+        #         index_file=INDEX_DIR / "index_surat_masuk.pkl"
+        #     )
+        #     self.search_engine = search_engine_service
+        
         self.search_engine.load()
         
         return self.search_engine.search(keyword)
